85ef8a76018d093cf0b08dede1ae0302c0f58e6d
lovinglifing <13929527132@163.com>
2019-06-03 16:16:05 +0800

    吴平16:15提交

--- src/main/java/com/qf/dto/TestDto.java
+++ src/main/java/com/qf/dto/TestDto.java
@@ -6,5 +6,6 @@ package com.qf.dto;
 public class TestDto {
     public static void main(String[] args) {
         System.out.println("hello world");
+        System.out.println("爱生活 爱自己");
     }
 }
